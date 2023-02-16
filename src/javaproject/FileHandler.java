623728b1bd26fcfa812db4af361c9807b7791dac
Almaz314 <almaz_gossov@mail.ru>
2023-02-16 17:48:11 +0300

    1

--- src/javaproject/FileHandler.java
+++ src/javaproject/FileHandler.java
@@ -1,4 +1,14 @@
 package javaproject;
 
+import java.util.LinkedList;
+import java.io.*;
+
 public class FileHandler {
+    public void readFile(LinkedList<>){
+        return ;
+    }
+    public void appendFile(){}
+
+    public void overWriteFile(){}
+
 }
