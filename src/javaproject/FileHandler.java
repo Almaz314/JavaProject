b482c09d610fa8f073db91cdd2a67d9d48f2c3bd
Almaz314 <almaz_gossov@mail.ru>
2023-02-17 16:29:29 +0300

    2

--- src/javaproject/FileHandler.java
+++ src/javaproject/FileHandler.java
@@ -4,9 +4,28 @@ import java.util.LinkedList;
 import java.io.*;
 
 public class FileHandler {
-    public void readFile(LinkedList<>){
-        return ;
+
+    public LinkedList<Member> readFile() {
+
+        LinkedList<Member> m = new LinkedList();
+        String lineRead;
+        String[] splitLine;
+        Member mem;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("member.cvs"))){
+            lineRead = reader.readLine();
+            while (lineRead != null) {
+                splitLine = lineRead.split(", ");
+
+            }
+            catch (IOException e){
+                    System.out.println(e.getMessage());
+                }
+                }
+            }
+        }
     }
+
     public void appendFile(){}
 
     public void overWriteFile(){}
